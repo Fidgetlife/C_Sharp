73faf8991875910f09765da1b0012596c7d3ac2e
Fidgetman <a@mail.ru>
2022-12-02 17:31:44 +0300

    solution task_8

---
+++ dz_1.8/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int start = 2;
+
+while(start <= num)
+{
+    Console.Write(start + ",");
+    start = start +2;
+}
