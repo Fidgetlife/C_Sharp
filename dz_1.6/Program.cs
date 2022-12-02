dccbba603b4848752805e00c8941a48cf6c72700
Fidgetman <a@mail.ru>
2022-12-02 17:21:29 +0300

    solution task_6

---
+++ dz_1.6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num % 2 != 0)
+{
+    Console.WriteLine("Это число не четное!");
+}
+else
+{
+    Console.WriteLine("Это число четное!");
+}
