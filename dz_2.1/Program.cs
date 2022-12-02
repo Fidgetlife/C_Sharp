0cd290b6e921d7d60d587d81491927b6263d6e48
Fidgetman <a@mail.ru>
2022-12-02 22:44:11 +0300

    solution task_2.1

---
+++ dz_2.1/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Введите трехзначное число: ");
+int numberEnter = Convert.ToInt32(Console.ReadLine());
+
+int secondDigitNumber = (numberEnter / 10) % 10;
+Console.WriteLine("Вторая цифра числа: " + secondDigitNumber);
