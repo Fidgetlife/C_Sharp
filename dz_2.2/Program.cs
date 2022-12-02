29c624900e97407a96e73124e416b563f21042ef
Fidgetman <a@mail.ru>
2022-12-02 23:00:49 +0300

    solution task_2.2

---
+++ dz_2.2/Program.cs
@@ -0,0 +1,12 @@
+﻿int randomNumber = new Random().Next(0, 99999);
+if(randomNumber / 100 >0)
+{
+    int thirdDigit = (randomNumber / 100) % 10;
+    Console.WriteLine(randomNumber);
+    Console.WriteLine("Третья цифра числа: " + thirdDigit);
+}
+else
+{
+    Console.WriteLine(randomNumber);
+    Console.WriteLine("У числа нет треьей цифры!");
+}
