86ed145b06ee457a5548e7bad8999440b423933e
Fidgetman <a@mail.ru>
2022-12-03 12:58:32 +0300

    solution task_3

---
+++ dz_2.3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число: ");
+int numberEnter = Convert.ToInt32(Console.ReadLine());
+if(numberEnter % 7 == 0 && numberEnter % 23 == 0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
