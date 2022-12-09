d5ce2eb24481d236a83a236cccd374afa3886d25
Fidgetman <a@mail.ru>
2022-12-09 15:48:51 +0300

    solution task_3.2

---
+++ dz_3.2/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Введите Х1:");
+float x1 = float.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите Y1:");
+float y1 = float.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите Z1:");
+float z1 = float.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите Х2:");
+float x2 = float.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите Y2:");
+float y2 = float.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите Z2:");
+float z2 = float.Parse(Console.ReadLine());
+
+float result;
+
+float arg(float coor1, float coor2) 
+{
+    float tearm =(float) Math.Pow((coor2 - coor1), 2);
+    return tearm;
+};
+
+result = (float)Math.Sqrt(arg(x1, x2) + arg(y1, y2) + arg(z1, z2));
+
+Console.WriteLine(result);
