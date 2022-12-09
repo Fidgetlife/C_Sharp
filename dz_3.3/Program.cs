7c01d745ffd75ed37852fb0bc8713e1ffca8269b
Fidgetman <a@mail.ru>
2022-12-09 16:56:04 +0300

    solution task_3.3

---
+++ dz_3.3/Program.cs
@@ -0,0 +1,15 @@
+﻿//int[] array = new int[] { 1, 2, 3 };
+//var str = string.Join(" ", array);
+//Console.WriteLine(str);
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[num];
+
+for(int i = 0; i < num; i++)
+{
+    array[i] = (int)Math.Pow(i+1, 3);
+}
+
+string str = string.Join(" ", array);    
+Console.WriteLine(str);
