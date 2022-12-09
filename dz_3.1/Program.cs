b9969d02a83062c5dce25e5553b8b527e0f895d3
Fidgetman <a@mail.ru>
2022-12-09 14:48:16 +0300

    solution task_3.1

---
+++ dz_3.1/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.WriteLine("Введите пятизначное число : ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[5] ;
+
+
+for(int i = 0; number > 0; i++)
+{
+    int num = number % 10;
+    array[i] = num; 
+    
+    number = number / 10;
+}
+
+int sizeArray = array.Length;
+int endIndArray = sizeArray - 1;
+
+for(int i = 0; i <= sizeArray / 2 ; i++)
+{
+    
+    if(array[i] != array[endIndArray])
+    {
+        Console.WriteLine("Нет, это число не является полиндромом!");
+        break;
+    }
+    
+    endIndArray--;
+
+    if(i == sizeArray / 2)
+    {
+        Console.WriteLine("Да, это число полиндром!");
+    }
+    
+}
+
+
