abf6a515f9f610af067243143434366b54646736
Fidgetman <a@mail.ru>
2022-12-10 23:52:05 +0300

    solution task_4.2

---
+++ dz_4.2/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int result = 0;
+int num = 0;
+int numberForCycle = number;
+
+while(numberForCycle != 0)
+{
+    num = numberForCycle % 10;
+
+    if(num == 0 || number % num == 0)  
+    {
+        result = result + num;
+    }
+
+    numberForCycle = numberForCycle / 10;
+
+}
+
+Console.Write($"Сумма цифр, делителей без остатка, входящих в состав числа '{number}' = " + result);
