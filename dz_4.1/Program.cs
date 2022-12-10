5a30dc4e57fdcb0e436a64e170d2a1030a60254a
Fidgetman <a@mail.ru>
2022-12-10 23:05:11 +0300

    solution task_4.1 used function

---
+++ dz_4.1/Program.cs
@@ -0,0 +1,21 @@
+﻿int Exponentiation(int num, int pow)
+{
+    int result = num;
+    for(int i = 1; i < pow; i++)
+    {
+        result = result * num;
+    }
+    
+    return result;
+}
+
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите степень числа, в которую нужно возвести: ");
+int powNumber = Convert.ToInt32(Console.ReadLine());
+
+
+int example = Exponentiation(number, powNumber);
+Console.Write($"Число '{number}' в степени '{powNumber}' равно: " + example);
