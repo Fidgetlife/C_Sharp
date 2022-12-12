13456d1df1bd72b12eb745395cfa08d76d0789ba
Fidgetman <a@mail.ru>
2022-12-12 03:19:10 +0300

    solution task_over_dz_medium_1.3

---
+++ over_dz_1.3/Program.cs
@@ -0,0 +1,46 @@
+﻿int GetNumber()
+{
+    int number = new Random().Next(0, 1000);
+    return number;
+}
+
+Console.Write("Введите длину массива:");
+int sizeArray = Convert.ToInt32(Console.ReadLine());
+
+int[] newArray = new int[sizeArray];
+for(int addressEl = 0; addressEl < sizeArray; addressEl++ )
+{
+    newArray[addressEl] = GetNumber();
+}
+
+
+int[] countArray = new int[sizeArray];
+
+for(int i = 0; i < sizeArray - 1; i++)
+{
+    if(newArray[i] < newArray[i+1]) 
+    {
+        countArray[i] = 1; 
+    }
+    else
+    {
+         countArray[i] = 0; 
+    }  
+}
+
+int count = 0;
+for(int y = 0; y < sizeArray-1; y++)
+{
+    if(newArray[y] > newArray[y + 1]) count++;
+}
+
+int el = 0;
+while(el < sizeArray)
+{
+    Console.Write($"{newArray[el]} ");
+    el++;
+}
+
+Console.WriteLine($"Число подъемов в массиве равно: {count}");
+
+// Нужно доработать. Есть нюансы.
