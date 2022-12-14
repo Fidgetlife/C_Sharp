33f74d205d96fa98fc07a1905f5d89e37bcc7a51
Fidgetman <a@mail.ru>
2022-12-14 16:02:03 +0300

    Add solution dz_5.1

---
+++ dz_5.1/Program.cs
@@ -0,0 +1,33 @@
+﻿int GetRandomNumber(int min, int max)
+{
+    Random number = new Random();
+    return number.Next(min, max);
+}
+
+Console.Write("Введите размер массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
+
+int countHonestNumber = 0;
+
+int[] array = new int[length];
+
+void FillArrayRandomNumber(int[] arr)
+{
+    for(int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = GetRandomNumber(100, 1000);
+    }
+}
+
+FillArrayRandomNumber(array);
+
+for(int el = 0; el < array.Length; el++)
+{
+    if(array[el] % 2 == 0) countHonestNumber++;
+}
+
+Console.Write(string.Join(" ", array));
+Console.WriteLine();
+Console.WriteLine("Колличество четных элементов в массиве = "+ countHonestNumber);
+
+
