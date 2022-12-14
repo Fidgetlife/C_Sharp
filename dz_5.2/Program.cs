378bda5b0d8ea026caafc3a093a932d4d6f460ca
Fidgetman <a@mail.ru>
2022-12-14 16:14:58 +0300

    Add solution dz_5.2

---
+++ dz_5.2/Program.cs
@@ -0,0 +1,31 @@
+﻿int GetRandomNumber(int min, int max)
+{
+    Random number = new Random();
+    return number.Next(min, max);
+}
+
+Console.Write("Введите размер массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
+
+int countSumHonestNumber = 0;
+
+int[] array = new int[length];
+
+void FillArrayRandomNumber(int[] arr)
+{
+    for(int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = GetRandomNumber(100, 1000);
+    }
+}
+
+FillArrayRandomNumber(array);
+
+for(int el = 0; el < array.Length; el++)
+{
+    if(el % 2 != 0 && array[el] % 2 == 0) countSumHonestNumber += array[el];
+}
+
+Console.Write(string.Join(" ", array));
+Console.WriteLine();
+Console.WriteLine("Сумма четных элементов в массиве стоящих на нечетных позициях = "+ countSumHonestNumber);
