d56bf8f6e455984a17c4f16a9d3eaf4ddea92e6b
Fidgetman <a@mail.ru>
2022-12-19 00:32:29 +0300

    Add solution dz_6.1

---
+++ dz_6.1/Program.cs
@@ -0,0 +1,49 @@
+﻿int countNumber = 0;
+int greatThanZero = 0;
+
+string str1 = "чисел";
+string str2 = "числа";
+
+string ReplaceWord(int num)
+{
+    if(num == 0 || num > 4)
+    {
+        return str1;
+    }
+    else
+    {
+        return str2;
+    }
+}
+
+void EnterNumber()
+{
+    Console.Write("Введите число. Если хотите прервать ввод, нажмите 'st'\n");
+    string str = Console.ReadLine();
+        
+    if(str != "st")
+    {
+        int num = Convert.ToInt32(str);
+        if(num > 0) 
+        {
+            countNumber++;
+            greatThanZero++;
+            EnterNumber();
+            
+        }
+        else
+        {
+            countNumber++;
+            EnterNumber();
+        }
+    }
+    else
+    {
+        return;
+    }
+    
+}
+
+EnterNumber();
+
+Console.Write($"Вы ввели '{countNumber}' {ReplaceWord(countNumber)}, из них больше '0' {greatThanZero} {ReplaceWord(greatThanZero)}");
