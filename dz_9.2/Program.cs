1a645b6e921afa901e86c781a83f6f819e5d0512
Fidgetman <a@mail.ru>
2023-01-16 15:33:02 +0300

    Add solution dz_9.2

---
+++ dz_9.2/Program.cs
@@ -0,0 +1,29 @@
+﻿/* Задача 2: Задайте значения M и N. Напишите программу, 
+которая найдёт сумму натуральных элементов в промежутке от M до N. Выполнить с помощью рекурсии.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30 */
+
+Console.Write("Введите число М: ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+
+void SummerNumber(int m, int n)
+{
+    if(m <= n)
+    {
+        result += m;
+        m += 1;
+        SummerNumber(m, n);
+    }
+}
+
+void OutputOnDisplay ()
+{
+    SummerNumber(numberM, numberN);
+    Console.Write(result);
+}
+
+OutputOnDisplay();
