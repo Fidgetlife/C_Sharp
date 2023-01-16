e23c8a50ce054203b9b71c8d704feb99e63c602a
Fidgetman <a@mail.ru>
2023-01-16 15:07:40 +0300

    Add solution dz_9.1

---
+++ dz_9.1/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Задача 1: Задайте значение N. Напишите программу, которая выведет
+ все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"*/
+
+Console.Write("Задайте значение N:  ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+void DecreaseNumber(int num)
+{
+    if(num != 0)
+    {
+        Console.Write($"{num}, ");
+        num -= 1;
+        DecreaseNumber(num);
+    }    
+}
+
+DecreaseNumber(numberN);
